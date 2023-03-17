4124d29e9a23eb6b0ad5dd2e8568977134fcb058
Evgeniy Kokaiko <70027656+re1nhart01@users.noreply.github.com>
2023-03-17 03:53:57 +0300

    [FME]-add-svg-and-locales

--- application/src/core/Navigation/MainNavigationScreen/view.tsx
+++ application/src/core/Navigation/MainNavigationScreen/view.tsx
@@ -1,17 +1,14 @@
-import React, { PropsWithChildren, useState } from 'react';
+import React, { PropsWithChildren, useRef } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { ScreenLayoutView } from '@components/hoc/ScreenLayout';
 
 export type mainNavigationPresenterProps = PropsWithChildren<{}>;
-
 const MainNavigationPresenter: React.FC<
   mainNavigationPresenterProps
 > = ({}) => {
   const Tab = createBottomTabNavigator();
   return (
-    <ScreenLayoutView backgroundColor="white">
-
-    </ScreenLayoutView>
+    <ScreenLayoutView backgroundColor="white" />
   );
 };
 
--- application/src/locale/i18next.ts
+++ application/src/locale/i18next.ts
@@ -1,9 +1,11 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import en from './en.json';
+import ua from './ua.json';
 
 const resources = {
   en: { translation: en },
+  ua: { translation: ua },
 };
 
 i18next
