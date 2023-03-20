5e426b16e819e5237ffdd60d12b00539f43c2b08
Evgeniy Kokaiko <70027656+re1nhart01@users.noreply.github.com>
2023-03-20 04:51:55 +0300

    Update response.go

--- server/pkg/utils/response.go
+++ server/pkg/utils/response.go
@@ -1,1 +1,33 @@
 package utils
+
+import "net/http"
+
+func GiveResponse(statusCode int, statusMessage string) map[string]any {
+	return map[string]any{
+		"statusCode":    statusCode,
+		"statusMessage": statusMessage,
+	}
+}
+
+func GiveResponseWithData[T comparable](statusCode int, statusMessage string, data T) map[string]any {
+	return map[string]any{
+		"statusCode":    statusCode,
+		"statusMessage": statusMessage,
+		"data":          data,
+	}
+}
+
+func GiveOKResponseWithData[T comparable](data T) map[string]any {
+	return map[string]interface{}{
+		"statusCode":    http.StatusOK,
+		"statusMessage": "Accepted",
+		"data":          data,
+	}
+}
+
+func GiveOKResponse() map[string]any {
+	return map[string]any{
+		"statusCode":    http.StatusOK,
+		"statusMessage": "Accepted",
+	}
+}
